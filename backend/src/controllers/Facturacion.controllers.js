cf8f899f9b4a5b8cd24d4d510933ff71d6cd745e
Jose <gradosj@gmail.com>
2020-11-27 16:01:25 +0100

    Creacion de modelos

---
+++ backend/src/controllers/Facturacion.controllers.js
@@ -0,0 +1,8 @@
+const FacturacionCtrl={}
+const Facturacion=require('../models/Facturacion.model');
+
+
+
+Facturacion.createFacturacion=async(req,res) => {
+    
+}
---
+++ backend/src/index.js
@@ -0,0 +1,23 @@
+const express=require('express');
+const app=express();
+const morgan=require('morgan');
+const bodyparser=require('body-parser');
+const cors=require('cors');
+
+require('./database');
+
+
+
+
+app.set('Port', 4001);
+
+
+app.use(morgan('dev'));
+app.use(bodyparser.urlencoded({extended:true}));
+app.use(bodyparser.json);
+app.use(cors({origin:'*'}));
+
+
+app.listen(app.get('Port'),() => {
+    console.log('server listening', app.get('Port'));
+})
