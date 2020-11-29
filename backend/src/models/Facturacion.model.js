70540c6dd77c155ed4818d796051153910c53c1d
Jose <gradosj@gmail.com>
2020-11-29 13:38:41 +0100

    Se añaden los scripts al .json y se añade clave unica al schema

--- backend/src/models/Facturacion.model.js
+++ backend/src/models/Facturacion.model.js
@@ -1,17 +1,13 @@
-const mongoose=require('mongoose');
-const {Schema} = mongoose;
+const mongoose = require("mongoose");
+const { Schema } = mongoose;
 
-
-const FacturacionSchema= new Schema({
-    factura: String,
-    nombreCliente: String,
-    fecha: String,
-    idFiscal: String,
-    consumo: String,
-    importe: String
+const FacturacionSchema = new Schema({
+  factura: { type: String, required: true, unique: true },
+  nombreCliente: String,
+  fecha: String,
+  idFiscal: String,
+  consumo: String,
+  importe: String,
 });
 
-
-module.exports = mongoose.model('facturacion',FacturacionSchema);
-
-
+module.exports = mongoose.model("facturacion", FacturacionSchema);
